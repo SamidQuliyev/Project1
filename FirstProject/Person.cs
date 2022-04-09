843e1f8ae209456d331e29aee8bce834b701d332
SamidQuliyev <samidquliyev2003@gmail.com>
2022-04-09 20:34:43 +0400

    init

---
+++ FirstProject/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Classes
+{
+        class Person
+    {
+
+        public string Name;
+        public string Surname;
+        public int Age;
+        public Person()
+        {
+            Console.WriteLine("Bir person yarandi");
+        }
+
+        public Person(string name, string surname):this()
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public Person(string name, string surname, int age) : this(name, surname)
+        {
+           
+            Age = age;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ FirstProject/Program.cs
@@ -0,0 +1,66 @@
+﻿using Access_Modifields;
+using System;
+namespace Access_Modifields
+{
+
+    class Program
+    {
+        //#region Static
+        //class Student
+        //{
+
+        //}
+        //public string Name { get; set; }
+        //public string Surname { get; set; }
+        //public int Age { get; set; }
+        //public student (string name,string surname,int age)
+        //{
+        //    Name = name;
+        //    Surname = surname;
+        //    Age = age;
+        //}
+
+
+
+        //#endregion
+        static void Main(string[] args)
+        {
+            //MyIntList myintList = new MyIntList();
+            int[] array = { 1, 2, 3 };
+            Array.Resize(ref array,array.Length+1);
+            array[3] = 4;
+            array[4] = 5;
+
+        }
+        class MyIntList
+        {
+            private int[] array;
+            public MyIntList()
+            {
+                array = new int[0];
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
