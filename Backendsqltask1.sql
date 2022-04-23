1d43a943882adcb26b82c3d1de9a4a509a7b46d5
SamidQuliyev <samidquliyev2003@gmail.com>
2022-04-23 16:21:29 +0400

    init

---
+++ Backendsqltask1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Employees( ID int identity primary key,Name nvarchar(50) not null,
+Surname nvarchar(100),Salary int not null)
+insert into Employees values ('Anar','Qurbanov',500),('Elmar','Babayev', 892),
+('Aysel','Eliyeva',651),('Ferhad','Resulzade',725),('Leyla','İmanov',498),
+('Saleh','İbrahimov',554)
+create table TestBool(Text nvarchar(10),Result bit)
+insert into TestBool values ('a',1),('b',0)
+select Text,Result from TestBool  --Bool tipi
+decimal --Kesrli ededleri yazmaq ucun  decimal(ededdeki max reqemlerin sayi,vergulden sonraki max reqemin sayi)
